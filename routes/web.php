1324af803e847d093f25b3b3043058042758f132
Koichiro Adachi <koichiro.adachi@smaregi.jp>
2021-02-01 23:08:47 +0900

    ルーティングに絶対パスを指定

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/board', 'BoardController@index');
+Route::get('/board', 'App\Http\Controllers\BoardController@index');
