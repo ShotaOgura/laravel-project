37dcffc550f206a54e9d5a232cb9d73bcb07088e
Shota <andy.1125.starrrrrrr@gmail.com>
2021-02-11 22:47:16 +0900

    1ちゃんねるの移行

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-
 class BoardController extends Controller
 {
     /**
@@ -10,6 +9,28 @@ class BoardController extends Controller
      */
     public function index()
     {
-        return view('board', ['name' => 'おぐおぐ']);
+
+        // DBから投稿のデータとってくる
+        $posts = \App\Models\Post::all();
+
+        // 画面に渡す
+        return view('board', ['posts' => $posts]);
+    }
+
+
+    public function create()
+    {
+        // DBアクセスするための道具を用意
+        $dsn =  'mysql:dbname=heroku_e6179ad37a6d96b;host=us-cdbr-east-03.cleardb.com;charset=utf8mb4';
+        $username = 'bc12c43204e33e';
+        $password = '04bfef93';
+        $pdo = new PDO($dsn, $username, $password);
+
+        $stmt = $pdo->prepare("INSERT INTO `posts` (`userName`, `posted`, `contents`) VALUES(?, NOW(), ?)");
+        $stmt->execute([$_POST["userName"], $_POST["contents"]]);
+
+        // 接続を閉じる
+        $pdo = null;
+
     }
 }
--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -1,6 +1,83 @@
 <!DOCTYPE html>
 <html>
+<header>
+<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
+<title>1ちゃんねる</title>
+</header>
     <body>
-        <h1>Hello, {{ $name }}</h1>
+        <h3 style="text-align:center">1ちゃんねる</h3>
+        <h5 style="text-align:center">日本最古のインターネット掲示板です。</h5>
+        <div class="post-history">
+            @foreach ($posts as $post)
+            <li class="frame post">
+                {{ $post->contents }}
+                <div class="post dt">{{ $post->posted}}</div><br>
+                <div class="post name">{{ $post->userName }}</div><br>
+            </li>
+        @endforeach
+        </div>
+
+        <div class="post-form">
+        <form action="post.php" method="post">
+            <label id="userName">名前</label>
+            <input id="userName" type="text" name="userName" maxlength="6">
+            <br>
+            <label id="contents">投稿内容</label>
+            <textarea id="contents" name="contents"></textarea>
+            <br>
+            <div style="text-align:center"><input type="submit" value="投稿する"></div>
+        </form>
+        </div>
+    <link rel="stylesheet" href="board.css">
     </body>
+
+    <style>
+    /* stylesheet */
+
+    li{
+        list-style:none ;
+    }
+
+    .frame {
+        width: 95%;
+        height: 100px;
+        background: #f5f5f5;
+        margin-left: auto;
+        margin-right: auto;
+        margin-bottom: 3px;
+        border:1px solid #000000;
+        }
+
+        .post {
+        position: relative;
+        }
+
+        .post.dt {
+            position: absolute;
+            bottom: 0px;
+            margin: 2px;
+        }
+
+        .post.name {
+        position: absolute;
+        bottom: 0px;
+        margin: 2px;
+        right: 2px;
+        }
+
+        .post-form {
+        width: 95%;
+        height: 100px;
+        margin-left: auto;
+        margin-right: auto;
+        margin-bottom: 3px;
+        border:1px solid #000000;
+        }
+
+        .post-form label{
+        vertical-align: top;
+        }
+
+    </style>
 </html>
+
